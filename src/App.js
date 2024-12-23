1e3f997d17d4f15081938d9ae144751bba1f9ef7
kapil <you@example.com>
2024-12-23 15:41:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Calculator from './Components/Calculator'; // Corrected import path
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
